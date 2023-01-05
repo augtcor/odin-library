d5ad2438d7e0a9f222806cbcc068cb2cbf6e20da
augcor <augusto.t.correa@gmail.com>
2023-01-05 13:50:30 -0300

    Fix radio button input

--- script.js
+++ script.js
@@ -10,13 +10,24 @@ function Book(title, author, pages, read) {
 const form = document.getElementById("form");
 const formAnswers = document.getElementById("formAnswers");
 
+//Open and close form to add new book
+const formOpenClose = document.getElementById("formOpenClose");
+formOpenClose.addEventListener("click", () => {
+	if (form.style.display === "none") {
+		form.style.display = "block";
+		formAnswers.reset();
+	} else {
+		form.style.display = "none";
+	}
+});
+
 //Add book to library
 const addBook = document.getElementById("addToLibrary");
 addBook.addEventListener("click", () => {
 	let title = document.getElementById("title").value;
 	let author = document.getElementById("author").value;
 	let pages = document.getElementById("pages").value;
-	let read = document.getElementById("read").value;
+	let read = document.querySelector('input[name="read"]:checked').value;
 
 	const book = new Book(title, author, pages, read);
 
@@ -30,16 +41,5 @@ addBook.addEventListener("click", () => {
 			formAnswers.reset();
 			return;
 		}
-	}
-});
-
-//Open and close form to add new book
-const formOpenClose = document.getElementById("formOpenClose");
-formOpenClose.addEventListener("click", () => {
-	if (form.style.display === "none") {
-		form.style.display = "block";
-		formAnswers.reset();
-	} else {
-		form.style.display = "none";
 	}
 });
