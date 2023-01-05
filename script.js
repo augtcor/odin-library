950352fd0078dc30bf0666db5708a6a778fcdc54
augcor <augusto.t.correa@gmail.com>
2023-01-05 11:00:11 -0300

    Create function to add books

--- script.js
+++ script.js
@@ -7,17 +7,35 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
-function addBookToLibrary() {
-	const book = new Book();
-}
+const form = document.getElementById("form");
+const formAnswers = document.getElementById("formAnswers");
+
+//Add book to library
+const addBook = document.getElementById("addToLibrary");
+addBook.addEventListener("click", () => {
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let pages = document.getElementById("pages").value;
+	let read = document.getElementById("read").value;
+
+	const book = new Book(title, author, pages, read);
+
+	console.log(myLibrary);
+
+	for (let key in book) {
+		if (book[key] === "") {
+			return;
+		} else {
+			myLibrary.push(book);
+			formAnswers.reset();
+			return;
+		}
+	}
+});
 
 //Open and close form to add new book
-const addBook = document.getElementById("addBook");
-
-addBook.addEventListener("click", () => {
-	const form = document.getElementById("form");
-	const formAnswers = document.getElementById("formAnswers");
-
+const formOpenClose = document.getElementById("formOpenClose");
+formOpenClose.addEventListener("click", () => {
 	if (form.style.display === "none") {
 		form.style.display = "block";
 		formAnswers.reset();
